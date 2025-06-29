afb1a4077f62573d8903d3f4f61c88ad2365c9f8
kmods <kmods@kali.kali>
2025-06-29 00:24:33 -0300

    Teste de commit v2

---
+++ public/estilo/js/scripts.js
@@ -0,0 +1,11 @@
+/*
+Venom na voz, essa base de site foi feito totalmente por mim
+base totalmente grátis para aprender fazer seu próprio sote de apis
+qualquer venda nessa source entre em contato comigo pelo WhatsApp ou telegram
+
+Se for postar ou usar deixe apenas os créditos 
+
+youtube.com/@venommodsss
+youtube.com/@venomkuromi
+wa.me/559784388524
+*/
